6752efd7fec797a289c8da845c7adbe7bba08c23
yhu98 <yhu98@ucsc.edu>
2017-01-27 17:17:09 -0800

    drag me

--- goBoard/src/app/app.module.ts
+++ goBoard/src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Draggable } from 'ng2draggable/draggable.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Draggable
   ],
   imports: [
     BrowserModule,
