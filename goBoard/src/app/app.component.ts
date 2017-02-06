9cabdd96af66814c5fcddcede64a8869cb33d5c0
Jeremy Chien <jchien15@gmail.com>
2017-02-05 18:39:37 -0800

    added random color generation

--- goBoard/src/app/app.component.ts
+++ goBoard/src/app/app.component.ts
@@ -8,10 +8,16 @@ import { Notes } from './notes';
 })
 export class AppComponent {
   notes = [];
+  colorlist = ['#ffd685', '#7aa9f7', '#99d3a9', '#f4a19a'];
+  randomNumber;
+  rcolor;
 
   onPress(note: string) {
+        this.randomNumber = Math.floor(Math.random() * this.colorlist.length);
+        this.rcolor = this.colorlist[this.randomNumber];
+        
         if (note){
-          this.notes.push(new Notes(note));
+          this.notes.push(new Notes(note, this.rcolor));
         }
         //console.log(JSON.stringify(note));
     }
