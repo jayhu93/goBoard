365c316614bcb3dfa7afbe6b38f40bd043ea8e20
Jeremy Chien <jchien15@gmail.com>
2017-02-05 21:20:33 -0800

    fixed duplicate note when pressing enter

--- goBoard/src/app/app.component.ts
+++ goBoard/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Notes } from './notes';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  notes = [];
+  public notes: Array<Notes> = [];
   colorlist = ['#FFCDD2', '#F8BBD0', '#E1BEE7', '#D1C4E9', '#C5CAE9', '#BBDEFB',
     '#B2EBF2', '#B2DFDB', '#C8E6C9', '#F0F4C3', '#FFECB3', '#FFE0B2', '#FFCCBC'];
   randomNumber;
@@ -17,9 +17,16 @@ export class AppComponent {
         this.randomNumber = Math.floor(Math.random() * this.colorlist.length);
         this.rcolor = this.colorlist[this.randomNumber];
         
-        if (note){
+        if (note)
           this.notes.push(new Notes(note, this.rcolor));
-        }
+
         //console.log(JSON.stringify(note));
     }
+/*
+    removeNote(note: Notes)
+    {
+      var index = this.notes.indexOf(note);
+      this.notes.splice(index, 1);
+    }
+    */
 }
